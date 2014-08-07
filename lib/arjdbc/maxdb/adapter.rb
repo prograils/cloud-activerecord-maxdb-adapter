8bde9131db4b86967457d1915b62b9977594b685
Jakub Szwachła <meszkax@gmail.com>
2014-08-07 15:13:11 +0200

    Convert boolean to tinyint

--- lib/arjdbc/maxdb/adapter.rb
+++ lib/arjdbc/maxdb/adapter.rb
@@ -37,7 +37,7 @@ module ::ArJdbc
       tp[:string] = { name: 'VARCHAR', limit: 255 }
       tp[:text] = { name: 'VARCHAR', limit: 5000 }
       tp[:integer] = { name: 'INTEGER', limit: nil }
-      tp[:boolean]     = { name: 'BOOLEAN' }
+      tp[:boolean]     = { name: 'TINYINT', limit: 1 }
       tp
     end
 
